8fb1267284aba3340c75ee94b87e5371c64ff5fc
coding-monk <rburnsme1@gmail.com>
2016-08-21 16:34:44 +0100

    added @RequestParam to take a parameter to add to the greeting message

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,16 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String index() {
-        return "Greetings from Spring Boot!";
+
+    @RequestMapping("/greeting")
+    public String index(@RequestParam(value="name", required=false, defaultValue="john") String name) {
+        return "hi "+ name+" Greetings from Spring Boot!";
     }
 
 }
